331c865892e57e87f8d08ed1d7f7078a1bad785c
Claire Gautier <claire.gautier@edhec.com>
2018-09-04 15:19:08 +0200

    created evet model and events controller + seed

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+  skip_before_action :authenticate_user!
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id, :title, :description)
+  end
+end
