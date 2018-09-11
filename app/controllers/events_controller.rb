2130bdf4684e9280f082ee6352edd03ed0a454fc
Claire Gautier <claire.gautier@edhec.com>
2018-09-11 18:08:05 +0200

    add policy for events destroy and update but not for reviews yet

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,31 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @events = Event.all
+    @events = policy_scope(Event)
   end
 
   def show
     @event = Event.find(params[:id])
+    authorize @event
     @review = Review.new
     @reviews = @event.reviews
   end
 
   def edit
     @event = Event.find(params[:id])
+    authorize @event
   end
 
   def update
     @event = Event.find(params[:id])
+    authorize @event
     @event.update(event_params)
     redirect_to event_path(@event)
   end
 
   def destroy
     @event = Event.find(params[:id])
+    authorize @event
     @event.destroy
     redirect_to root_path
   end
---
+++ app/policies/event_policy.rb
@@ -0,0 +1,29 @@
+class EventPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_admin?
+  end
+
+  def destroy?
+    user_is_admin?
+  end
+
+  private
+
+  def user_is_admin?
+    user.admin
+  end
+end
