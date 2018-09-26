44d274192579df25951668d5afa016ba0869cae0
Claire Gautier <claire.gautier@edhec.com>
2018-09-26 17:31:24 +0200

    sylvain fixed navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,7 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
-  if (navbar) {
+  if (navbar && navbar.classList.contains('navbar-scroll')) {
+    console.log("here")
     window.addEventListener('scroll', (e) => {
       if (window.scrollY >= window.innerHeight - 120) {
         navbar.classList.add('navbar-wagon-scrolled');
