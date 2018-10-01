ee9c9f6db5289d4f04681a308ba6b26c5995bfaa
Claire Gautier <claire.gautier@edhec.com>
2018-10-01 18:16:51 +0200

    front of alert messages and edit registration form and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks',
+      registrations: 'users/registrations'
+    }
   root to: 'events#index'
   resources :events, shallow: true, only: [:show, :edit, :update, :destroy ] do
     resources :reviews, only: [:create, :edit, :update, :destroy] do
