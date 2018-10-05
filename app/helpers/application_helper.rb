79b8523630ab860ec49677bc99b81c16c51b6694
Claire Gautier <claire.gautier@edhec.com>
2018-10-05 15:40:01 +0200

    hover not visible on mobile deviec solved ans capitalize after apostrophe location name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def capitalize_apostrophe(str)
+    # location.name.split(/ |\_/).map(&:capitalize).join(" ")
+    str.capitalize.gsub(/[' ][a-z]/, &:upcase)
+  end
 end
+
